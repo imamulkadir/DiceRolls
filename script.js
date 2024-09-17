e614d83d21193dad36636d89d8f97c722b5272ed
Imamul Kadir <imamul.kadir@wsd.com>
2024-09-18 01:05:39 +0600

    updated

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ btnHold.addEventListener('click', function () {
       scores[activePlayer];
 
     //2. Check if players score is =>100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       //Finish the game
       playing = false;
       diceEl.classList.add('hidden');
